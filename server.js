0c53efabce3c00c2219e3870ecfda5e1b01a0367
Steve Magana <steveamagana@gmail.com>
2023-06-03 22:11:13 -0700

    Add authentication and email logins with passport

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const passport = require('passport')
+const flash = require('express-flash')
 const logger = require('morgan')
 const connectDB = require('./config/database')
 const mainRoutes = require('./routes/main')
@@ -8,11 +10,20 @@ const todoRoutes = require('./routes/todos')
 
 require('dotenv').config({path: './config/.env'})
 
+// Passport config
+require('./config/passport')(passport)
+
 connectDB()
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(logger('dev'))
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(flash())
 
 app.use('/', mainRoutes)
 app.use('/todos', todoRoutes)
