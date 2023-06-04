3c1851fb1b2bbd4ebc4101ecb3fdd7bea55dc932
Steve Magana <steveamagana@gmail.com>
2023-06-04 01:17:17 -0700

    Add sessions

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const passport = require('passport')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
 const flash = require('express-flash')
 const logger = require('morgan')
 const connectDB = require('./config/database')
@@ -18,6 +20,16 @@ connectDB()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(logger('dev'))
+
+// Sessions
+app.use(
+    session({
+      secret: 'keyboard cat',
+      resave: false,
+      saveUninitialized: false,
+      store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    })
+)
 
 // Passport middleware
 app.use(passport.initialize())
